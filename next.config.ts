835744b2c5c1ce0d79e2e3d947a7b2e95ad2e49d
Doomster13 <83833113+Doomster13@users.noreply.github.com>
2026-07-27 16:58:39 +0530

    default

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,15 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: path.join(__dirname),
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/auth/login",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
